4fb8daabb8d48938e0dd39fbaa19dd5ff8caeb76
kamil27272727 <kamilish27@gmail.com>
2024-07-16 17:50:01 +0600

    weyirwuei

---
+++ core/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Music
+
+
+def index_views(request):
+    musics = Music.objects.all()
+
+    return render(request, 'app/index.html', {'musics': musics})
+
+
+def music_detail_view(request, pk):
+
+    music = Music.objects.get(id=pk)
+
+    return render(request, 'app/music_detail.html', {'music': music})
